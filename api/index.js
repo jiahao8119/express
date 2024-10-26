9f9e475c46a40268647582cce23af185317135ba
yap <jiahaoyap9139@gmail.com>
2024-10-27 02:22:08 +0800

    update

--- api/index.js
+++ api/index.js
@@ -8,6 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(cors({
+    origin: ['https://final-fullstack-vert.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
